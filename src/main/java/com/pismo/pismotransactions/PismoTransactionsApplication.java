284455b05f8a5d6de85ea348a183bcac6076fb8e
vieiralucas89 <vyeiralucas@gmail.com>
2022-11-05 15:04:34 -0300

    first commit

---
+++ src/main/java/com/pismo/pismotransactions/PismoTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.pismo.pismotransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PismoTransactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PismoTransactionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pismo/pismotransactions/PismoTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pismo.pismotransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PismoTransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
